de96a746a0bec1c3939f64da88148ccd5de6fd21
zapalap <przemyslaw.zapala@gmail.com>
2018-10-12 10:43:23 +0200

    Readd Models folder

---
+++ Zapalap.GeoChat.Api/Models/BotResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zapalap.GeoChat.Models
+{
+    public class BotResponse
+    {
+        public string Cs { get; set; }
+        public string Output { get; set; }
+    }
+}
